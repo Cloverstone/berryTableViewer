3d6aa703c373faa028ebe48b911eb3feacb5c08e
Adam Smallcomb <atsmallcomb@gmail.com>
2016-10-11 16:20:03 -0400

    Compiled, improved ui

--- src/js/bread.js
+++ src/js/bread.js
@@ -237,13 +237,13 @@ function berryTable(options) {
 					_.each(checked_models, function(item){item.checked = false;})	
 
 					this.draw();							
-					this.$el.find('[data-event="select_all"] .fa').attr('class', 'fa fa-lg fa-square-o');
+					this.$el.find('[data-event="select_all"] .fa').attr('class', 'fa fa-fw fa-lg fa-square-o');
 
 				}else{
 					_.each(this.filtered, function(item){item.checked = true;})					
 					this.draw();
 
-					this.$el.find('[data-event="select_all"] .fa').attr('class', 'fa fa-lg fa-check-square-o');
+					this.$el.find('[data-event="select_all"] .fa').attr('class', 'fa fa-fw fa-lg fa-check-square-o');
 
 
 				}
@@ -253,8 +253,8 @@ function berryTable(options) {
 			this.berry = $el.find('.form').berry({attributes: options,inline:true, actions: false, fields: [
 					{label:'Entries per page', name:'count', type: 'select',default:{label: 'All', value: 10000}, options: options.entries || [25, 50 ,100] , columns: 2},
 					{label:false,name:"reset",type:'raw',value:'<button name="reset-search" class="btn btn-default btn-sm" style="margin-top: 30px;"><i class="fa fa-filter"></i>  Reset Filter</button>',columns: 2},
-					{label:false,name:"reset",type:'raw',value:'<button data-event="add" class="btn btn-success pull-right btn-sm" style="margin-top: 30px;"><i class="fa fa-pencil-square-o"></i> Create New</button>',columns: 2,show:!!(options.add)},
 					// {label: 'Search', name:'filter', columns: 5, offset: 1, pre: '<i class="fa fa-search"></i>'}
+					{label:false,name:"reset",type:'raw',value:'<button data-event="add" class="btn btn-success pull-right btn-sm" style="margin-top: 30px;"><i class="fa fa-pencil-square-o"></i> Create New</button>',columns: 2,offset:6,show:!!(options.add)},
 				]}).on('change:count', function(){
 				$.extend(options, this.berry.toJSON());
 				options.count = parseInt(options.count,10);
@@ -279,11 +279,11 @@ function berryTable(options) {
 						this.$el.find('.paginate-footer').html(templates['table_footer'].render(this.renderObj,templates));
 						var checkbox = this.$el.find('[data-event="select_all"] .fa');
 						if(this.renderObj.checked_count == this.models.length){
-							checkbox.attr('class', 'fa fa-lg fa-check-square-o');
+							checkbox.attr('class', 'fa fa-lg fa-fw fa-check-square-o');
 						}else if(this.renderObj.checked_count == 0){
-							checkbox.attr('class', 'fa fa-lg fa-square-o');
+							checkbox.attr('class', 'fa fa-lg fa-fw fa-square-o');
 						}else{
-							checkbox.attr('class', 'fa fa-lg fa-minus-square-o');
+							checkbox.attr('class', 'fa fa-lg fa-fw fa-minus-square-o');
 						}
 
 					}.bind(this))
